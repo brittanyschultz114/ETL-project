adb71617b5d102fbff7c26d22b9ffefb35d3ec04
RCC0149 <75042560+RCC0149@users.noreply.github.com>
2021-03-11 01:09:39 -0500

    Add files via upload

--- schemata.sql
+++ schemata.sql
@@ -1,15 +1,15 @@
---ALTER TABLE "links" 
-	--ADD PRIMARY KEY ("movieId");
+ALTER TABLE "links" 
+	ADD PRIMARY KEY ("movieId");
 		
---ALTER TABLE "movies"
-	--ADD PRIMARY KEY ("id");	
+ALTER TABLE "movies"
+	ADD PRIMARY KEY ("id");	
 	
---ALTER TABLE "links" 
-	--ADD CONSTRAINT "fk_links_tmdbId"
-		--FOREIGN KEY("tmdbId")
-			--REFERENCES "movies" ("id");
+ALTER TABLE "links" 
+	ADD CONSTRAINT "fk_links_tmdbId"
+		FOREIGN KEY("tmdbId")
+			REFERENCES "movies" ("id");
 
---ALTER TABLE "ratings"
-	--ADD CONSTRAINT "fk_ratings_movieId"
-		--FOREIGN KEY("movieId")
-			--REFERENCES "links" ("movieId");
+ALTER TABLE "ratings"
+	ADD CONSTRAINT "fk_ratings_movieId"
+		FOREIGN KEY("movieId")
+			REFERENCES "links" ("movieId");
