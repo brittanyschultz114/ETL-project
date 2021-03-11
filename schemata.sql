db7931c73d83f4c8cc8bfb60851f36dffcb9aea1
RCC0149 <75042560+RCC0149@users.noreply.github.com>
2021-03-11 03:30:37 -0500

    Column Names Adjusted

--- schemata.sql
+++ schemata.sql
@@ -1,15 +1,15 @@
 ALTER TABLE "links" 
-	ADD PRIMARY KEY ("movieId");
+	ADD PRIMARY KEY ("movieid");
 		
 ALTER TABLE "movies"
 	ADD PRIMARY KEY ("id");	
 	
 ALTER TABLE "links" 
-	ADD CONSTRAINT "fk_links_tmdbId"
-		FOREIGN KEY("tmdbId")
+	ADD CONSTRAINT "fk_links_tmdbid"
+		FOREIGN KEY("tmdbid")
 			REFERENCES "movies" ("id");
 
 ALTER TABLE "ratings"
-	ADD CONSTRAINT "fk_ratings_movieId"
-		FOREIGN KEY("movieId")
-			REFERENCES "links" ("movieId");
+	ADD CONSTRAINT "fk_ratings_movieid"
+		FOREIGN KEY("movieid")
+			REFERENCES "links" ("movieid");
